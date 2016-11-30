64058ab94f022158e9887012ba73b885123883f9
Rickard Sunden <rickard.sunden@outlook.com>
2016-11-29 21:09:36 -0500

    add example transitions from vue guide

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,6 +2,18 @@
   <div id="about">
     <p class="main"> well I'm just learning how this stuff works </p>
     <p class="main"> {{ msg }} </p>
+    <button @click="show = !show">
+      Toggle
+    </button>
+    <transition
+      v-on:before-enter="beforeEnter"
+      v-on:enter="enter"
+      v-on:leave="leave"
+      v-bind:css="false">
+      <p v-if="show">
+        Demo
+      </p>
+    </transition>
   </div>
 </template>
 
@@ -10,7 +22,27 @@ export default {
   name: 'about',
   data () {
     return {
-      msg: "this text is coming from vue"
+      msg: "this text is coming from vue",
+      show: false
+    }
+  },
+  methods: {
+    beforeEnter: function (el) {
+      el.style.opacity = 0
+    },
+    enter: function (el, done) {
+      Velocity(el, { opacity: 1, fontSize: '1.4em' }, { duration: 300 })
+      Velocity(el, { fontSize: '1em' }, { complete: done })
+    },
+    leave: function (el, done) {
+      Velocity(el, { translateX: '15px', rotateZ: '50deg' }, { duration: 600 })
+      Velocity(el, { rotateZ: '100deg' }, { loop: 2 })
+      Velocity(el, {
+        rotateZ: '45deg',
+        translateY: '30px',
+        translateX: '30px',
+        opacity: 0
+      }, { complete: done })
     }
   }
 }
